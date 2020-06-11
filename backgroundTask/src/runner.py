a9e235de253bea43d558ab086e5e3e60b10e89a4
LeeHemingway <coherence0815@gmail.com>
2020-06-12 08:59:46 +0900

    update init

--- backgroundTask/src/runner.py
+++ backgroundTask/src/runner.py
@@ -1,3 +1,30 @@
+import os, time
+import psycopg2
+import traceback
 
-print("hello, task")
+def run(conn):
+    print("hello, task")
 
+def setup():
+    print("setup")
+
+    user = os.environ["DBUSER"]
+    pwd = os.environ["DBPASS"]
+    host = os.environ["DBHOST"]
+    dbname = os.environ["DBNAME"]
+
+    try:
+        conn = psycopg2.connect(f"dbname='{dbname}' user='{user}' host='{host}' password='{pwd}'")
+        print("connected")
+    except:
+        print(traceback.format_exc())
+    
+    while True:
+        run(conn)
+        time.sleep(3)
+
+if __name__ == "__main__":
+    try:
+        setup()
+    except:
+        print(traceback.format_exc())
